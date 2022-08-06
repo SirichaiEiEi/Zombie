14a0db90c97f66d88ce9cc5939e99f3364ac2cd2
SirichaiEiEi <108708170+SirichaiEiEi@users.noreply.github.com>
2022-08-06 15:09:52 +0700

    finis2

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,22 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     public AudioSource sr;
     public AudioClip ac;
+    public GameObject Zombie;
     void Start()
     {
         sr = GetComponent<AudioSource>();
         ac = GetComponent<AudioClip>();
+        Zombie = GetComponent<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
-            sr.PlayOneShot(ac);
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.name.CompareTo("Zombie") == 0)
+                {
+                    sr.Play();
+                }
+            }
+        }
     }
 }
